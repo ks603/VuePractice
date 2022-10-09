ba6917ffcaa3ee81beb2f9662bb44ee17c120a19
Kevin Shaughnessy <kevin.shaughnessy18@gmail.com>
2022-10-09 15:12:08 -0500

    Working with conditional directives in Vue

--- vue3_practice/src/App.vue
+++ vue3_practice/src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <h1> Hey Vue3 from CLI </h1>
-  <P> something else </p>
+  <h1 v-if="isShowing" v-html="title"></h1>
+  <h3 v-else-if="isShowing === null">This is v-else-if</h3>
+  <h2 v-else>This is the v-else part</h2>
+  <p> something else </p>
 </template>
 
+<script>
+export default {
+  data(){
+    return {
+      title: "Heyy Vue3",
+      isShowing: true,
+    };
+  },
+};
+</script>
 
+ 
