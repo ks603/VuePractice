81ccf002674787b67fa9055e8d20eb50c4b27a5f
Kevin Shaughnessy <kevin.shaughnessy18@gmail.com>
2022-10-10 15:16:48 -0500

    For loop with Vue

--- vue3_practice/src/App.vue
+++ vue3_practice/src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <h1 v-if="isShowing" v-html="title"></h1>
-  <h3 v-else-if="isShowing === null">This is v-else-if</h3>
-  <h2 v-else>This is the v-else part</h2>
-  <p> something else </p>
+  <h1>DC Heroes</h1>
+  <ul>
+    <li v-for="num in 10" :key="num.id">{{num}}</li>
+  </ul>
 </template>
+
 
 <script>
 export default {
-  data(){
-    return {
-      title: "Heyy Vue3",
-      isShowing: true,
-    };
-  },
-};
+
+}
 </script>
 
- 
+<style>
+
+</style>
