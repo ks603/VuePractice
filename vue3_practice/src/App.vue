462afc9ed20c976fbacaf96352174e41b412b5ee
Kevin Shaughnessy <kevin.shaughnessy18@gmail.com>
2022-10-12 19:10:11 -0500

    Resolved root element error

--- vue3_practice/src/App.vue
+++ vue3_practice/src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <h1>DC Heroes</h1>
   <ul>
-    <li v-for="num in 10" :key="num.id">{{num}}</li>
+    <h1>DC Heroes</h1>
+    <li v-for="(value, index) in dcHeros" :key="value.id">{{ index }}{{ value }} {{ key }}</li>
   </ul>
 </template>
 
 
 <script>
 export default {
-
+  data() {
+    return {
+      dcHeros: {SuperGirl: 'SuperGirl', Flash: 'Flash',Batman: 'Batman', Arrow: 'Arrow', SuperMan: 'SuperMan'},
+    }
+  }
 }
 </script>
 
